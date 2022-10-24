ec03e664cf0c3fb31e574c897856211bcbb6f8f7
Brian Lovell <brian.lovell@outlook.com>
2022-10-24 14:17:30 -0400

    [Modify] comment move

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -101,34 +101,34 @@ async function handleCreateRace() {
 
 async function runRace(raceID) {
 	try {
-	return new Promise(resolve => {
-	// TODO - use Javascript's built in setInterval method to get race info every 500ms	
-		let raceInterval = setInterval(() => getRace(raceID),500)
-		
-		if (raceInterval.status == 'in-progress') {
+		return new Promise(resolve => {
+		// TODO - use Javascript's built in setInterval method to get race info every 500ms	
+				
+		/* 
+			TODO - if the race info status property is "in-progress", update the leaderboard by calling:
+
 			renderAt('#leaderBoard', raceProgress(res.positions))
-		}
-		if (raceInterval.status == 'finished') {
-			clearInterval(raceInterval)
-			renderAt('#race', resultsView(res.positions))
-			resolve(res)
-		}
-	
-	/* 
-		TODO - if the race info status property is "in-progress", update the leaderboard by calling:
+		*/
 
-		renderAt('#leaderBoard', raceProgress(res.positions))
-	*/
+		/* 
+			TODO - if the race info status property is "finished", run the following:
 
-	/* 
-		TODO - if the race info status property is "finished", run the following:
-
-		clearInterval(raceInterval) // to stop the interval from repeating
-		renderAt('#race', resultsView(res.positions)) // to render the results view
-		reslove(res) // resolve the promise
-	*/
-	})
-	// remember to add error handling for the Promise
+			clearInterval(raceInterval) // to stop the interval from repeating
+			renderAt('#race', resultsView(res.positions)) // to render the results view
+			reslove(res) // resolve the promise
+		*/
+			let raceInterval = setInterval(() => getRace(raceID),500)
+			
+			if (raceInterval.status == 'in-progress') {
+				renderAt('#leaderBoard', raceProgress(res.positions))
+			}
+			if (raceInterval.status == 'finished') {
+				clearInterval(raceInterval)
+				renderAt('#race', resultsView(res.positions))
+				resolve(res)
+			}
+		})
+		// remember to add error handling for the Promise
 	} catch(error) {
 		console.log(error)
 	}
